bd183a5e3bbda4fc1a2abbfd4c0a91b365eae85d
KrishCodesw <krishjain.w@gmail.com>
2026-01-24 01:39:39 +0530

    garib ki kalakaari ep-4

--- frontend/app/queryexplainer/page.tsx
+++ frontend/app/queryexplainer/page.tsx
@@ -401,7 +401,7 @@ export default function QueryExplainer({
                   {/* Card: Tables */}
                   <div className="bg-white p-5 rounded-2xl border border-slate-200 shadow-sm hover:shadow-md transition-shadow">
                     <div className="flex items-center gap-3 mb-4">
-                      <div className="p-2  bg-violet-50 text-black/80 rounded-lg">
+                      <div className="p-2  bg-violet-50  text-black/80 rounded-lg">
                         {/* <Database className="w-5 h-5" /> */}
                         <Table className="w-5 h-5" />
                       </div>
