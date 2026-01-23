7fb5ee6c8b8cbf1e55b820e5caaa7b33922fdac0
KrishCodesw <krishjain.w@gmail.com>
2026-01-23 12:09:55 +0530

    explainer  endpoint working

--- venv/src/main.py
+++ venv/src/main.py
@@ -4,7 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from src.ai.gemini import Gemini
 from src.ai.querycorrector import QueryCorrector
-from typing import List, Optional, Any
+from src.ai.queryexplainer import QueryExplainer
+from typing import List, Optional
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -35,6 +36,15 @@ def load_query_corrector_prompt():
 querycorrector_prompt=load_query_corrector_prompt()
 
 query_corrector_platform = QueryCorrector(api_key=gemini_api_key, query_correctorsystemprompt=querycorrector_prompt)
+
+def load_query_explainer_system_prompt():
+        base_dir=os.path.dirname(os.path.abspath(__file__))
+        prompt_path=os.path.join(base_dir,"prompts","queryexplainer_systemprompt.md")
+        with open(prompt_path,encoding="utf-8") as f:
+            return f.read()
+
+queryexplainer_prompt=load_query_explainer_system_prompt()
+query_explainer_platform=QueryExplainer(api_key=gemini_api_key,queryexplainer_systemprompt=queryexplainer_prompt)
 
 app=FastAPI()
 
@@ -85,3 +95,37 @@ async def correct_query(request: QueryCorrectorRequest):
         raise ValueError("QueryCorrector did not return a valid dict response")
 
     return QueryCorrectorResponse(**result)
+
+
+
+class QueryExplainerRequest(BaseModel):
+    sql_query: str
+
+class SummaryModel(BaseModel):
+    purpose: str
+    complexity_level: str
+
+class AggregationModel(BaseModel):
+    grouping_criteria: str
+    metrics_calculated: str
+
+class DetailedAnalysisModel(BaseModel):
+    tables_involved: List[str]
+    filters_applied: List[str]
+    joins: List[str]
+    aggregation: AggregationModel
+    final_output: str
+
+class QueryExplainerResponse(BaseModel):
+    summary: SummaryModel
+    detailed_analysis: DetailedAnalysisModel
+    risks_and_notes: List[str]
+
+
+@app.post("/explainquery",response_model=QueryExplainerResponse)
+async def explainquery(request:QueryExplainerRequest):
+    result=query_explainer_platform.chat(request.sql_query)
+    if not isinstance(result, dict):
+        raise ValueError("QueryExplainer did not return a valid dict response")
+
+    return QueryExplainerResponse(**result)
